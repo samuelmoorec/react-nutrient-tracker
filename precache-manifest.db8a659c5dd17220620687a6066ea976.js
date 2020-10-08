ddcfd10b2d17c86a3d13c7b8aace339d97d234f5
Samuel C Moore <samuel.c.moore1221@gmail.com>
2020-10-08 09:44:37 -0500

    Updates

---
+++ precache-manifest.db8a659c5dd17220620687a6066ea976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f3fbd34a7763b513e2c7ac82268658",
+    "url": "/react-nutrient-tracker/index.html"
+  },
+  {
+    "revision": "ab40274483ffb299a078",
+    "url": "/react-nutrient-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ba1ebe092f59a30dcdd",
+    "url": "/react-nutrient-tracker/static/js/2.3791a31a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-nutrient-tracker/static/js/2.3791a31a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab40274483ffb299a078",
+    "url": "/react-nutrient-tracker/static/js/main.e54183d3.chunk.js"
+  },
+  {
+    "revision": "3bf6b17b966b08fd5a30",
+    "url": "/react-nutrient-tracker/static/js/runtime-main.5bed4392.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-nutrient-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
